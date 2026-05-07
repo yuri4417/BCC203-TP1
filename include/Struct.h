0bb9e4a7ecf2b573626d8a460fd98f22e7d5989d
pedrohenriquefonsecapeixoto <pedrohpeixotoprofissional@gmail.com>
2026-05-07 11:47:41 -0300

    Alterações_ArvoreB_07_05_CorrecaoGit

--- include/Struct.h
+++ include/Struct.h
@@ -29,27 +29,16 @@ typedef struct {
     TipoItem item;
     int dir;
 } ItemABB;
-<<<<<<< HEAD
+
 typedef TipoItem TipoRegistro;
-=======
->>>>>>> ecf1304ba0b63ce1c8a41218570b0994d79aee8f
 
 typedef TipoItem TipoRegistro;
 typedef struct TipoPagina* TipoApontador;
 
 typedef struct TipoPagina {
     short n;
-<<<<<<< HEAD
     TipoRegistro r[MM];
     TipoApontador p[MM + 1];
 } TipoPagina;
-
-=======
-    TipoRegistro r[2 * M];
-    TipoApontador p[2 * M + 1];
-} TipoPagina;
-
-
->>>>>>> ecf1304ba0b63ce1c8a41218570b0994d79aee8f
 
 #endif // STRUCT_H
